9b70ff650e7c4b99d3bfc4d87bbf7d441dad2492
Colin Alexander <chalexander@me.com>
2019-08-24 18:15:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,18 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  original_library = YAML.load_file(file_path)
+  p original_library
+  emoticon_library["get_meaning"] = original_library.reduce({}) do |memo, (key, value)|
+      memo[value[1]] = key
+      memo
+    end
+  
+  
+  
+  
+  emoticon_library["get_emoticon"] = {}
+  emoticon_library
 end
 
 def get_japanese_emoticon
