6be2c01fe0aadfbe913c5a6fa9d95d841e46cf16
Colin Alexander <chalexander@me.com>
2019-08-24 18:17:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@ require "yaml"
 
 def load_library(file_path)
   original_library = YAML.load_file(file_path)
-  p original_library
+  emoticon_library = {}
   emoticon_library["get_meaning"] = original_library.reduce({}) do |memo, (key, value)|
       memo[value[1]] = key
       memo
